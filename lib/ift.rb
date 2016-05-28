36e20a75edb4588b102d1043da4772916062f79e
ross mulcare <ross.mulcare@gmail.com>
2016-05-27 21:39:56 -0400

    Finish and format IFT chart

--- lib/ift.rb
+++ lib/ift.rb
@@ -6,16 +6,22 @@ class IFT
   def initialize
     fp = [1, 2, 4, 6, 8, 12, 16, 20, 24, 30, 36]
     @ift = [
-      ["1KIA", "2KIA", "2KIA", "3KIA", "3KIA,", "3KIA", "4KIA", "4KIA", "5KIA", "6KIA", "7KIA"],
-      ["K/1", "1KIA", "1KIA", "2KIA", "2KIA", "2KIA", "3KIA", "3KIA", "4KIA", "5KIA", "6KIA"],
-      ["1MC", "K/1", "K/2", "1KIA", "1KIA", "1KIA", "2KIA", "2KIA", "3KIA", "4KIA", "5KIA", "6KIA"],
-      ["1MC", "1MC", "2MC", "K/2", "K/2", "K/3", "1KIA", "1KIA", "2KIA", "2KIA", "3KIA", "4KIA", "5KIA"],
-      ["NMC", "1MC", "1MC", "2MC", "2MC", "3MC", "K/3", "K/4", "1KIA", "2KIA", "3KIA", "4KIA"],
-      ["PTC", "NMC", "1MC", "1MC", "2MC", "2MC", "3MC", "4MC", "K/4", "1KIA", "2KIA"],
-      ["NR", "PTC", "NMC", "1MC", "1MC", "2MC" "2MC", "3MC", "4MC", "K/4", "1KIA"],
-      ["NR", "NR", "PTC", "NMC", "1MC", "1MC", "2MC", "2MC", "3MC", "4MC", "K/4"],
-      ["NR", "NR", "NR", "PTC", "NMC", "1MC", "1MC", "2MC", "2MC", "3MC", "4MC"],
-      ["NR", "NR", "NR", "NR", "PTC", "NMC", "1MC", "1MC", "2MC", "2MC", "3MC"]
+      ["1KIA",  "2KIA", "2KIA", "3KIA", "3KIA", "3KIA", "4KIA", "4KIA", "5KIA", "6KIA", "7KIA"],
+      ["K/1",   "1KIA", "1KIA", "2KIA", "2KIA", "2KIA", "3KIA", "3KIA", "4KIA", "5KIA", "6KIA"],
+      ["1MC",   "K/1",  "K/2",  "1KIA", "1KIA", "1KIA", "2KIA", "2KIA", "3KIA", "4KIA", "5KIA"],
+      ["1MC",   "1MC",  "2MC",  "K/2",  "K/2",  "K/3",  "1KIA", "1KIA", "2KIA", "3KIA", "4KIA"],
+      ["NMC",   "1MC",  "1MC",  "2MC",  "2MC",  "3MC",  "K/3",  "K/4",  "1KIA", "2KIA", "3KIA"],
+      ["PTC",   "NMC",  "1MC",  "1MC",  "2MC",  "2MC",  "3MC",  "4MC",  "K/4",  "1KIA", "2KIA"],
+      ["NR",    "PTC",  "NMC",  "1MC",  "1MC",  "2MC"   "2MC",  "3MC",  "4MC",  "K/4",  "1KIA"],
+      ["NR",    "NR",   "PTC",  "NMC",  "1MC",  "1MC",  "2MC",  "2MC",  "3MC",  "4MC",  "K/4"],
+      ["NR",    "NR",   "NR",   "PTC",  "NMC",  "1MC",  "1MC",  "2MC",  "2MC",  "3MC",  "4MC"],
+      ["NR",    "NR",   "NR",   "NR",   "PTC",  "NMC",  "1MC",  "1MC",  "2MC",  "2MC",  "3MC"],
+      ["NR",    "NR",   "NR",   "NR",   "NR",   "PTC",  "NMC",  "1MC",  "1MC",  "2MC",  "2MC"],
+      ["NR",    "NR",   "NR",   "NR",   "NR",   "NR",   "PTC",  "NMC",  "1MC",  "1MC",  "2MC"],
+      ["NR",    "NR",   "NR",   "NR",   "NR",   "NR",   "NR",   "PTC",  "NMC",  "1MC",  "1MC"],
+      ["NR",    "NR",   "NR",   "NR",   "NR",   "NR",   "NR",   "NR",   "PTC",  "NMC",  "1MC"],
+      ["NR",    "NR",   "NR",   "NR",   "NR",   "NR",   "NR",   "NR",   "NR",   "PTC",  "NMC"],
+      ["NR",    "NR",   "NR",   "NR",   "NR",   "NR",   "NR",   "NR",   "NR",   "NR",   "PTC"]
      ]
   end
 
